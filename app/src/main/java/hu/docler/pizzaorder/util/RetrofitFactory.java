5930719b0d53317149a98a08a7fa6a7280854df4
Ferenc Knebl <ferenc.knebl@cognex.com>
2018-04-11 15:06:29 +0200

    updated app - cache policy

--- app/src/main/java/hu/docler/pizzaorder/util/RetrofitFactory.java
+++ app/src/main/java/hu/docler/pizzaorder/util/RetrofitFactory.java
@@ -19,23 +19,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitFactory {
 
-//    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
-//        @Override
-//        public Response intercept(Chain chain) throws IOException {
-//            Response originalResponse = chain.proceed(chain.request());
-//            if (Utils.isNetworkAvailable(context)) {
-//                int maxAge = 60; // read from cache for 1 minute
-//                return originalResponse.newBuilder()
-//                        .header("Cache-Control", "public, max-age=" + maxAge)
-//                        .build();
-//            } else {
-//                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
-//                return originalResponse.newBuilder()
-//                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-//                        .build();
-//            }
-//        }
-//    }
+    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Response originalResponse = chain.proceed(chain.request());
+            int maxAge = 60 * 60; // read from cache for 1 hour
+            return originalResponse.newBuilder()
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .build();
+        }
+    };
 
     public static final String BASE_URL = "https://api.myjson.com/";
 
@@ -54,7 +47,7 @@ public class RetrofitFactory {
         Cache cache = new Cache(httpCacheDirectory, cacheSize);
 
         OkHttpClient client = new OkHttpClient.Builder()
-                //.addNetworkInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
+                .addNetworkInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
                 .cache(cache)
                 .build();
 
